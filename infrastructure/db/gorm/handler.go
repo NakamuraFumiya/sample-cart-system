9a14e9efbddb55fd0d7e42eec219eb18aaf212ee
NakamuraFumiya <fumiya3691@gmail.com>
2025-05-14 22:45:19 +0900

    gorm.DBを取得する関数をinfra層に定義

---
+++ infrastructure/db/gorm/handler.go
@@ -0,0 +1,15 @@
+package gorm
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func GetInstance() (*gorm.DB, error) {
+	dsn := ""
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}

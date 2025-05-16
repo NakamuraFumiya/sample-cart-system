9cec3609f1eb6a9ee721dbfc1736a49042a7fb8a
NakamuraFumiya <fumiya3691@gmail.com>
2025-05-16 09:31:27 +0900

    FIXMEコメントを追加

--- infrastructure/db/gorm/handler.go
+++ infrastructure/db/gorm/handler.go
@@ -7,6 +7,8 @@ import (
 
 var gormHandler *gorm.DB
 
+// FIXME: スレッドセーフになっていないので修正する
+// FIXME: 環境変数から読めるようにする
 func GetInstance() (*gorm.DB, error) {
 	if gormHandler == nil {
 		dsn := "development_user:development_password@tcp(127.0.0.1:3306)/development_database?charset=utf8mb4&parseTime=True&loc=Local"

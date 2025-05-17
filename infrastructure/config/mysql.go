c23f40ea323c5f5d5a8a9069f66e4ab65fb03afd
NakamuraFumiya <fumiya3691@gmail.com>
2025-05-17 13:03:23 +0900

    tweak

---
+++ infrastructure/config/mysql.go
@@ -0,0 +1,9 @@
+package config
+
+type MySQLConfig struct {
+	Database string
+	User     string
+	Password string
+	Host     string
+	Port     uint16
+}
--- infrastructure/config/viper/handler.go
+++ infrastructure/config/viper/handler.go
@@ -1,19 +1,34 @@
 package viper
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"github.com/fumiyanakamura/sample-cart-system/infrastructure/config"
 	"github.com/spf13/viper"
 )
 
-func Load() {
+func Load() config.MySQLConfig {
 	viper.SetConfigName("local")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("config")
 	if err := viper.ReadInConfig(); err != nil {
 		panic(fmt.Errorf("fatal error config file: %w", err))
 	}
-	fmt.Println("================")
-	fmt.Println(viper.Get("rdb"))
-	fmt.Println("================")
+
+	// map → JSON
+	// JSONにエンコードする
+	jsonData, err := json.Marshal(viper.Get("rdb.mysql"))
+	if err != nil {
+		panic(fmt.Errorf("json marshal error: %w", err))
+	}
+
+	// JSON → struct
+	// 構造体にデコードする
+	var mysqlConfig config.MySQLConfig
+	if err := json.Unmarshal(jsonData, &mysqlConfig); err != nil {
+		panic(fmt.Errorf("json unmarshal error: %w", err))
+	}
+
+	return mysqlConfig
 }
